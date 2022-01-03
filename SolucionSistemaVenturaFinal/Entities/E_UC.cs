cc51fe2295cf89535405fd3d1b5e7b49adef39c6
Maloko <marlon1428@gmail.com>
2022-01-03 15:56:23 -0500

    aaa

---
+++ SolucionSistemaVenturaFinal/Entities/E_UC.cs
@@ -0,0 +1,23 @@
+using System;
+namespace Entities
+{
+    public class E_UC
+    {
+        public string CodUc { get; set; }
+        public decimal ContadorAcum { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int FlagActivo { get; set; }
+        public string HostCreacion { get; set; }
+        public string HostModificacion { get; set; }
+        public int IdPerfil { get; set; }
+        public int IdPerfilNeumatico { get; set; }
+        public string IdTipoUnidad { get; set; }
+        public int IdUc { get; set; }
+        public int IdUsuarioCreacion { get; set; }
+        public int IdUsuarioModificacion { get; set; }
+        public string Observacion { get; set; }
+        public string PlacaSerie { get; set; }
+        public int IdEstadoUC { get; set; }
+    }
+}
